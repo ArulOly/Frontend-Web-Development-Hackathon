317e9fff06376acfec54d827d476931240d169bd
ArulOly <279612773+ArulOly@users.noreply.github.com>
2026-08-03 19:13:00 +0800

    Initialize App component with MindMap integration

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import MindMap from "./MindMap";
+
+function App() {
+  return (
+    <div style={{ height: "100vh", width: "100vw" }}>
+      <h1 style={{ textAlign: "center", margin: "10px" }}>🧠 MindMap Generator</h1>
+      <MindMap />
+    </div>
+  );
+}
+
+export default App;
